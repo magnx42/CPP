c41e70e967e0f8b128350db7f0b9e8faa729b9ce
Matis Geneix <geneixmatis@gmail.com>
2026-08-11 10:06:24 +0200

    cpp07 ex02 constructeurs Array (defaut, taille, copie)

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -5,7 +5,7 @@
 template <typename T>
 class Array{
 private:
-    unsigned int size;
+    unsigned int _tabSize;
     T *t;
 public:
     Array();
@@ -21,6 +21,23 @@ public:
     operator[](unsigned int size) const;
 };
 
+template <typename T>
+Array<T>::Array() : _tabSize(0), t(new T[0])
+{
+}
 
+template <typename T>
+Array<T>::Array(unsigned int n) : _tabSize(n), t(new T[n]())
+{
+}
+
+template <typename T>
+Array<T>::Array(const Array& other)
+{
+	this->t = new T[other._tabSize];
+	this->_tabSize = other._tabSize;
+	for(unsigned int i = 0;i < _tabSize;i++)
+		this->t[i] = other.t[i];
+}
 
 #endif
